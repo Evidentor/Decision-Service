3c7475528c3234a8bc5c85252349f024d3c2e820
Karlito16 <karlo.dimjasevic@gmail.com>
2025-05-25 21:01:18 +0200

    Initial project setup

---
+++ src/test/java/net/dimjasevic/karlo/fer/evidentor/decision_service/DecisionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dimjasevic.karlo.fer.evidentor.decision_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
